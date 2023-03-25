754f3a6a7807d0acfc45097095f6ca8fecdd3c78
Hillkip23 <128432591+Hillkip23@users.noreply.github.com>
2023-03-25 09:26:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import random
 import altair as alt
 import numpy as np
 import pandas as pd
+from vega_datasets import data
 
 st.header('Homework 1')
+
 
 st.markdown(
 "**QUESTION 1**: In previous homeworks you created dataframes from random numbers.\n"
@@ -36,18 +38,11 @@ df = pd.DataFrame({'x': x_axis,
 st.write(df)
 
 
-
-
-
-
-
-
-
-
 st.markdown(
 "**QUESTION 2**: Using the dataframe you just created, create a basic scatterplot and Print it.\n"
 "Use the following code block to help get you started."
 )
+
 
 st.code(
 ''' 
@@ -55,16 +50,16 @@ scatter = alt.Chart().mark_point().encode()
 
 st.altair_chart(scatter, use_container_width=True)''',language='python')
 
-scatter = alt.chart(df).mark_point().encode(
-    x='x:Q',
-    y='y:Q',
-    color = 'Origin:N'
+scatter = alt.Chart(df).mark_point().encode(
+    x='x',
+    y='y'
 )
 
 st.altair_chart(scatter, use_container_width=True)
-st.code(
-   
-)
+
+
+
+
 st.markdown(
 "**QUESTION 3**: Lets make some edits to the chart by reading the documentation on Altair.\n"
 "https://docs.streamlit.io/library/api-reference/charts/st.altair_chart.  "
@@ -76,17 +71,23 @@ st.markdown(
 st.markdown("The five changes I made were.....")
 st.markdown("""
 The 5 changes I made were:
-- Change 1
-- Change 2
-- Change 3
-- Change 4
-- Change 5
+- Change 1: Tooltip added
+- Change 2: x axias label
+- Change 3: y axis label
+- Change 4: Chart title
+- Change 5,6,7,8: Size, Color, Opacity, Fill
 """)
 
+scatter = alt.Chart(df,title='My new chart title').mark_point(size=100, opacity=.9, fill='green', color='green').encode(
+    alt.X('x',title="my new x-axis title"),
+    alt.Y('y',title="My new y-axis"),
+    tooltip = ['x', 'y']
+    )
 
+st.altair_chart(scatter, use_container_width=True)
 
 st.markdown(
-"**QUESTION 4**: Explore on your own!  Go visit https://altair-viz.github.io/gallery/index.html.\n "
+"**QUESTION 4**: Explore on your own!  Go visit https://altair-viz.github.io/gallery/index.html. \n "
 "Pick a random visual, make two visual changes to it, document those changes, and plot the visual.  \n"
 "You may need to pip install in our terminal for example pip install vega_datasets "
 )
@@ -97,3 +98,16 @@ The 2 changes I made were:
 - Change 2
 """
 )
+
+
+source = pd.read_json('imdb.json')
+st.write(source)
+
+bar = alt.Chart(source).mark_bar().encode(
+    alt.X("IMDB_Rating:Q", bin=True),
+    y='count()',
+)
+
+st.altair_chart(bar, use_container_width=True)
+
+
